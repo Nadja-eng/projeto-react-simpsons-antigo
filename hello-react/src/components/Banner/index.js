55ce96f519cd803be06a3a68216cbf6205b24239
Nadja Eloy <ndja2011@yahoo.com.br>
2021-06-24 18:48:04 -0300

    firt commit

---
+++ hello-react/src/components/Banner/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import img  from '../../assets/imagem.svg'
+
+import './styles.css'
+
+const Banner = () => {
+    return(
+         <div className="main">
+            <div>
+                <h1>Meu site divertido</h1>
+                <p>Site feito em React para treinar react-router-dom e axios</p>
+            </div>
+            <img src={img} alt="ilustracao com logo do react" />
+        </div>
+    )
+}
+export default Banner
